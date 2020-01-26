c3b056e0acdde456a6b039f9c6deeb9063b0429e
Hannes Thaller <hannes.thaller.at@gmail.com>
2020-01-26 20:33:47 +0100

    Added the extract task. Added the clean task

--- tasks.py
+++ tasks.py
@@ -1,10 +1,59 @@
 import pathlib
+import re
+import shutil
 
 import pandas as pd
+from invoke import task
 
 DIR_THIS = pathlib.Path(__file__).parent
+DIR_BUILD = pathlib.Path(DIR_THIS, "build")
 FILE_INPUT = pathlib.Path(DIR_THIS, "gcj2017-input.csv.gz")
 FILE_SOURCE = pathlib.Path(DIR_THIS, "gcj2017-source.csv.gz")
 
 df_input = pd.read_csv(FILE_INPUT)
 df_source = pd.read_csv(FILE_SOURCE)
+
+
+@task(default=True)
+def extract(c, round=None, category=None, task=None, solution=None, max=10, per_task=True, seed=14):
+    assert max > 0
+    df = pd.read_csv(FILE_SOURCE)
+    df = _filter_sources(df, round, category, task, solution)
+
+    if df.shape[0] > 0:
+        if per_task:
+            df = (df.groupby(["Round", "RoundCategory", "Task"])
+                  .apply(lambda x: x.sample(min(max, len(x)), random_state=seed)))
+        else:
+            df = df.sample(max, random_state=seed)
+
+        for it in df.itertuples():
+            key = f"{it.Round}-{it.RoundCategory}-{it.Task}-{it.Solution}"
+            dir_problem = pathlib.Path(DIR_BUILD, key)
+            dir_solution = pathlib.Path(dir_problem, it.DeveloperId)
+
+            dir_solution.mkdir(parents=True, exist_ok=True)
+
+            content = re.sub("package .*;", "", it.FileContent)
+            with pathlib.Path(dir_solution, it.File).open("w") as files:
+                files.write(content)
+    else:
+        print(f"No valid solution for given filters: "
+              f"round={round}, category={category}, task={task}, solution={solution}")
+
+
+def _filter_sources(df, round, category, task, solution) -> pd.DataFrame:
+    if round is not None:
+        df = df[df.Round == round]
+    if category is not None:
+        df = df[df.RoundCategory == category]
+    if task is not None:
+        df = df[df.Task == task]
+    if solution is not None:
+        df = df[df.Solution == solution]
+    return df
+
+
+@task
+def clean(c):
+    shutil.rmtree(DIR_BUILD)
