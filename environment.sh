dbbb28184830825db098392d08a0f993215e7c55
Hannes Thaller <hannes.thaller.at@gmail.com>
2020-01-28 15:06:33 +0100

    Removed obsolete source fiels.
    Removed duplicate solution entries

--- environment.sh
+++ environment.sh
@@ -2,7 +2,8 @@
 
 echo "Creating conda environment"
 
+source ~/miniconda3/etc/profile.d/conda.sh
 conda create -n gradient-dataset-clones -c conda-forge python=3.6 -y
 conda activate gradient-dataset-clones
-conda install -c conda-forge invoke=1.3 pandas=0.25
+conda install -y -c conda-forge invoke=1.3 pandas=0.25
 exit 0
--- tasks.py
+++ tasks.py
@@ -11,27 +11,27 @@ DIR_THIS = pathlib.Path(__file__).parent
 DIR_BUILD = pathlib.Path(DIR_THIS, "build")
 FILE_INPUT = pathlib.Path(DIR_THIS, "gcj2017-input.csv.gz")
 FILE_SOURCE = pathlib.Path(DIR_THIS, "gcj2017-source.csv.gz")
+KEY_COLUMNS = ["Round", "RoundCategory", "Task", "Solution"]
 
 df_input = pd.read_csv(FILE_INPUT)
 df_source = pd.read_csv(FILE_SOURCE)
 
 
-@task(default=True)
-def extract(c, round=None, category=None, task=None, solution=None, max=10, per_task=True, seed=14):
+@task(name="source", default=True)
+def source_extract(c, round=None, category=None, task=None, solution=None, max=10, per_task=True, seed=14):
     assert max > 0
-    groupby_cols = ["Round", "RoundCategory", "Task", "Solution"]
 
     df = pd.read_csv(FILE_SOURCE)
-    df = _filter_sources(df, round, category, task, solution)
+    df = _source_filter(df, round, category, task, solution)
 
     if df.shape[0] > 0:
         if per_task:
-            df = (df.groupby(groupby_cols)
+            df = (df.groupby(KEY_COLUMNS)
                   .apply(lambda x: x.sample(min(max, len(x)), random_state=seed)))
         else:
             df = df.sample(max, random_state=seed)
 
-        groupby_getter = operator.attrgetter(*groupby_cols)
+        groupby_getter = operator.attrgetter(*KEY_COLUMNS)
         for groupby_key, rows in itertools.groupby(df.itertuples(), groupby_getter):
             key = "-".join([str(it) for it in groupby_key])
             dir_problem = pathlib.Path(DIR_BUILD, key)
@@ -41,14 +41,14 @@ def extract(c, round=None, category=None, task=None, solution=None, max=10, per_
                 dir_solution.mkdir(parents=True, exist_ok=True)
 
                 content = re.sub("package .*;", "", it.FileContent)
-                with pathlib.Path(dir_solution, it.File).open("w") as files:
-                    files.write(content)
+                with pathlib.Path(dir_solution, it.File).open("w") as f:
+                    f.write(content)
     else:
         print(f"No valid solution for given filters: "
               f"round={round}, category={category}, task={task}, solution={solution}")
 
 
-def _filter_sources(df, round, category, task, solution) -> pd.DataFrame:
+def _source_filter(df, round, category, task, solution) -> pd.DataFrame:
     if round is not None:
         df = df[df.Round == round]
     if category is not None:
@@ -58,6 +58,44 @@ def _filter_sources(df, round, category, task, solution) -> pd.DataFrame:
     if solution is not None:
         df = df[df.Solution == solution]
     return df
+
+
+@task(name="update")
+def source_update(c, round=None, category=None, task=None, solution=None, developer=None):
+    df = pd.read_csv(FILE_SOURCE).set_index(KEY_COLUMNS + ["DeveloperId"])
+
+    dir_solution = pathlib.Path(DIR_BUILD, f"{round}-{category}-{task}-{solution}/{developer}")
+
+    df_key = (round, category, task, int(solution), developer)
+    if df_key in df.index:
+        file_solution = pathlib.Path(dir_solution, df.loc[df_key].iloc[0].File)
+        if file_solution.exists():
+            with file_solution.open("r") as f:
+                df.at[df_key, "FileContent"] = f.read()
+
+            df.reset_index().to_csv(FILE_SOURCE, index=False)
+        else:
+            print("Solution cannot be updated because the new solution file does not exist.")
+    else:
+        print("Solution cannot be updated because the target entry does not exist.")
+
+
+@task(name="trigger")
+def trigger_extract(c, round=None, category=None, task=None, solution=None):
+    df = pd.read_csv(FILE_INPUT)
+    df = _source_filter(df, round, category, task, solution)
+
+    for it in df.itertuples():
+        dir_problem = pathlib.Path(DIR_BUILD, f"{it.Round}-{it.RoundCategory}-{it.Task}-{it.Solution}")
+        dir_problem.mkdir(parents=True, exist_ok=True)
+
+        with pathlib.Path(dir_problem, "input.txt").open("w") as f:
+            f.write(it.Input)
+
+
+@task(name="run")
+def run(c, round=None, category=None, task=None, solution=None, developer=None):
+    pass
 
 
 @task
